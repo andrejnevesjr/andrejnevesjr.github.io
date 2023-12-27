62b152bf0d31d83b485f8653a3a44b5b3e46ba9c
André Neves Junior <andre.nevesjunior@scopely.com>
2023-12-27 11:01:57 +0000

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-const currentDate = new Date().getFullYear();
-
-export default class Footer extends Component {
-  render() {
-    return <> Copyright &copy; {currentDate} - André Junior</>;
-  }
-}
